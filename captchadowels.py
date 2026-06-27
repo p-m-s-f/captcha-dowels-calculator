f6957f4825284a6ffbdf28631c7845e4adc25738
Parris Mook-Sang-Forbes <parris@validmind.ai>
2026-06-27 15:21:15 -0700

    Uploading WIP captchadowels script to github

---
+++ captchadowels.py
@@ -0,0 +1,82 @@
+import sys # importing sys so we can use sys.exit() when parsing input
+CAPTCHA_VALS = []
+
+def get_input():
+    '''
+    Prompts the user to paste the image URL for today's Flash
+    Gordon strip, and returns their input as a string.
+    '''
+    print("Type or paste your captchalogue code:")
+    inputString = str(input())
+
+    return inputString
+
+def create_list_of_captcha_values():
+    '''
+    Creates a list of characters comprising Homestuck's captcha code cipher,
+    representing numbers 0-63 in ascending order.
+    The first 62 characters -- actually the complete list of alphanumeric
+    characters in lexicographically ascending order  (i.e.
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+    -- represent numeric values 0-61. The final two characters ("?" and "!")
+    represent numbers 62 and 63.
+
+    We can retrieve the numerical value associated with a given character by
+    retrieving that character's list index, e.g. 0=0, 1=1... A=10, and so on.
+    '''
+    numericalChars = [str(n) for n in list(range(0,10))]
+
+    from string import ascii_uppercase as upCaseAlphabet
+    lowCaseAlphabet = upCaseAlphabet.casefold()
+    alphabeticalChars = list(upCaseAlphabet + lowCaseAlphabet + "?!")
+
+    listOfCaptchaVals = numericalChars + alphabeticalChars
+    return listOfCaptchaVals
+
+def process_input(input):
+    '''
+    check input is correct, and if it is, return input as list of characters-as-strings
+    '''
+    #Check that input is correct length
+    lenInput = len(input)
+    if lenInput != 8:
+        if lenInput > 8:
+            print("Error: Your captchalogue code is too long. Check your code and try again.")
+        else:
+            print("Error: Your captchalogue code is too short. Check your code and try again.")
+        sys.exit(1)
+
+    listOfInput = list(input)
+    for c in listOfInput:
+        if c not in CAPTCHA_VALS:
+            print("Error: Your captchalogue code contains an imcompatible character. Check your code and try again.")
+            sys.exit(1)
+
+    return listOfInput
+
+
+def get_captcha_vals(input):
+    '''
+    Takes captchalogue code as list of characters (strings), returns
+    a list of ints corresponding to each character's captcha cipher value
+    '''
+    return []
+
+
+def main():
+    '''
+    TODO:
+    1. collect input
+    4. feed processed input to function that translates to dowel widths
+    TODO: maybe just write an error message function? 
+    '''
+    global CAPTCHA_VALS
+    CAPTCHA_VALS = create_list_of_captcha_values()
+
+    input = get_input()
+    input = process_input(input)
+
+    inputCaptchaVals = get_captcha_vals(input)
+
+if __name__ == "__main__":
+    main()
